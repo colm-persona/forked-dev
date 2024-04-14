d68830ee1675a8e2122ab6ea04ca1113d15c4955
colmpat <cplang@dons.usfca.edu>
2024-04-14 14:00:33 -0700

    update env

--- scripts/update-env.sh
+++ scripts/update-env.sh
@@ -3,6 +3,7 @@ mkdir -pv env/.config
 # copy config dirs
 for dir in nvim tmux fish; do
   cp -R ~/.config/$dir/ env/.config/$dir
+  rm -rf env/.config/$dir/.git # remove git artifacts
 done
 
 # copy config files
