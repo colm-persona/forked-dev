86b3373448f878b43460b42d288fb78286c22a3b
colmpat <cplang@dons.usfca.edu>
2024-04-14 12:34:38 -0700

    add fish script

---
+++ scripts/fish.sh
@@ -0,0 +1,14 @@
+# Install fish shell and set it as default shell
+if "$(uname)" == "Darwin"; then
+  brew install fish
+else if "$(uname)" == "Linux"; then
+  sudo apt-add-repository ppa:fish-shell/release-3
+  sudo apt-get update
+  sudo apt-get install fish
+else
+  echo "Unsupported OS"
+  exit 1
+fi
+
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
